2b55b55ea4cc8b3d3608f48a033cad2208749cb1
崔凯 <cuikai@xiaoneng.cn>
2016-08-11 13:13:39 +0800

    百度编辑器UEditor   Mini版-----------UMeditor插件添加

--- Application/Admin/Controller/ContentController.class.php
+++ Application/Admin/Controller/ContentController.class.php
@@ -1,13 +1,22 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-//权限管理模块
+//内容管理模块
 class ContentController extends CommonController {
 
 
-
+	//内容管理列表
     public function index(){
 		$this->display();
     }
-
+	
+	//内容添加
+	public function addContent(){
+		if(IS_POST){
+			var_dump($_POST);
+		}else{
+			$this->display();
+		}
+    }
+	
 }
