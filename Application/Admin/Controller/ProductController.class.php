b5d65c575839eede228b009a39c77e73fa0eee05
961900940 <961900940@qq.com>
2016-08-16 22:08:16 +0800

    --------------26.2

--- Application/Admin/Controller/ProductController.class.php
+++ Application/Admin/Controller/ProductController.class.php
@@ -9,32 +9,36 @@ class ProductController extends CommonController {
 	public function index(){
 		$Newscontent =  M('Content');
 		$content = $Newscontent->where("id=3")->find();
-		$content['content'] = htmlspecialchars_decode($content['content']);
+		$content['content'] = ($content['content']);
 		$this->assign('content',$content);
         $this->display();
     }
-	
+
 	public function addProduct(){
-		$content = I("content"); 
-		$data['content'] = $content;
-		$data['updatetime'] = date('Y-m-d H:i:s',time());
-		$Newscontent =  M('Content');
-		$res = $Newscontent->add($data);
-		if($res){
-			$this->success('添加成功,即将跳转...', U('Product/index'),3);
-		}else{
-			$this->error('添加失败，请重试...');
-		}
+        if(IS_POST){
+            $content = I("content");
+    		$data['content'] = htmlspecialchars_decode($content);
+    		$data['updatetime'] = date('Y-m-d H:i:s',time());
+    		$Newscontent =  M('Content');
+    		$res = $Newscontent->add($data);
+    		if($res){
+    			$this->success('添加成功,即将跳转...', U('Product/index'),3);
+    		}else{
+    			$this->error('添加失败，请重试...');
+    		}
+        }else{
+            $this->display();
+        }
     }
-	
+
 	/*public function lists(){
         $list=M("article")->find();
         $this->content=htmlspecialchars_decode($list['content']);
         $this->display();
-    
+
    }
-	
-	
+
+
     public function index(){
         $Newscontent =  M('Content');
 		$content = $Newscontent->where("id=3")->find();
--- Public/README.md
+++ Public/README.md
@@ -11,6 +11,11 @@
 			|-js			
 			|-images		
 		|-static		静态资源公共目录（第三方）
-			|-bootstrap		bootstrap-3.3.5-dist		
-			|-miniui		版本v3.7
+			|-bootstrap					bootstrap-3.3.5-dist		
+			|-miniui					版本v3.7
+			|-kindeditor				版本	4.1.4
+			|-kindeditor-4.1.4			版本	4.1.4
+			|-layer-v2.3				版本2.3(web弹层组件)	
+			|-ueditor1_4_3_3-utf8-php	版本1.4.3.3(百度编辑器)
+			|-umeditor1_2_2-utf8-php		版本1.2.2
 			|- ...			公用js
